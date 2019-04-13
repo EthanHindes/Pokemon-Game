28bfa6f1ad5cd89555baba679d55a46247be4a94
hindese <Offlin01000101>
2019-04-13 08:46:25 -0400

    Tested the pokemon structure and it works!! I've also added in a jar to run everything to make it simpler!

--- src/msoe/hindese/pokemongame/Controller.java
+++ src/msoe/hindese/pokemongame/Controller.java
@@ -1,42 +1,80 @@
 /*
- * Course: CS2852 - 071
+ * Course: Side Project
  * Spring 2018-2019
- * Lab TODO
+ * Project - Pokemon Game
  * Name: Ethan Hindes
- * Created: TODO
+ * Created: 04.12.2019
  */
 
 package msoe.hindese.pokemongame;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * TODO
+ * This class controls the GUI application
+ *
  * @author Ethan Hindes
- * @version TODO
+ * @version 04.13.2019
  */
 public class Controller implements Initializable {
 
-    Map map;
+    private Map map;
 
     @FXML
     TextArea playerField;
 
     /**
-     * TODO
+     * This method responds to the user moving across the map
+     *
      * @param key that was pressed
      */
     @FXML
     private void changeMap(KeyEvent key) {
-        playerField.setText(map.updateMap(key.getCharacter().toLowerCase().charAt(0)));
+//        char move = ' ';
+//        switch(key.getCode()) {
+//            case W:
+//            case UP:
+//                move = 'W';
+//                break;
+//            case A:
+//            case LEFT:
+//                move = 'A';
+//                break;
+//            case S:
+//            case DOWN:
+//                move = 'S';
+//                break;
+//            case D:
+//            case RIGHT:
+//                move = 'D';
+//                break;
+//        }
+//        if(move != ' ') {
+//            playerField.setText(map.updateMap(move));
+//        }
+        playerField.setText(map.updateMap(key.getCharacter().charAt(0)));
     }
 
+    /**
+     * This method shuts down the application
+     */
+    @FXML
+    private void close() {
+        Platform.exit();
+    }
+
+    /**
+     * This method loads the map and sets the GUI
+     *
+     * @param url NA
+     * @param resourceBundle NA
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         char[][] charGrid = {
